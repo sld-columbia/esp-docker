daa726346e6bfe65e77e742c2eb65574036d7e0e
Davide Giri <davide_giri@cs.columbia.edu>
2020-10-20 10:39:22 -0400

    centos7 full, small: add missing packages, utils and env scripts
    - add missing packages for ESP: perl-Env, minicom, socat, tmux
    - add bashrc, emacs, tmux scripts
    - add environment variables script with CAD tools
    - update environment variables script without CAD tools

--- centos7/full/esp_env.sh
+++ centos7/full/esp_env.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+#
+# Set ESP environment variables (no CAD tools)
+#
+
+# xilinx vivado environment
 export XILINX_VIVADO=/
+
+# risc-v toolchain environment
 export RISCV=/home/espuser/riscv
 export PATH=$PATH:/home/espuser/riscv/bin
+echo "Exported RISC-V toolchain environment"
+
+# leon3 toolchain environment
 export PATH=$PATH:/home/espuser/leon/bin
 export PATH=$PATH:/home/espuser/leon/mklinuximg
 export PATH=$PATH:/home/espuser/leon/sparc-elf/bin
+echo "Exported Leon3 toolchain environment"
